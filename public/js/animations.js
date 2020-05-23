d6080c4e89dd78e65640337d5d29376488d3801c
unknown <alexander.norberghoff@gmail.com>
2020-05-23 16:12:12 +0200

    added projects

---
+++ public/js/animations.js
@@ -0,0 +1,36 @@
+const headings = document.querySelectorAll('.about__header, .projects__header')
+const gallery = document.querySelectorAll('.projects__grid')
+const filterbtns = document.querySelectorAll('.filterbtn')
+const skills = document.querySelectorAll('.skill')
+
+const observer = new IntersectionObserver((entries, self) => {
+   entries.forEach(entry => {
+      if (entry.isIntersecting) {
+         const el = entry.target
+         if (el.classList.contains('skill')) {
+            if (el.dataset.n % 2 == 0) {
+               el.classList.add('animateRight')
+            } else {
+               el.classList.add('animateLeft')
+            }
+         } else if (el.classList.contains('filterbtn')) {
+            el.style.animation = `fadeIn 1.5s ${el.dataset.delay}s ease forwards`
+         } else if (el.classList.contains('projects__grid')) {
+            el.classList.add('animateUp')
+         } else {
+            el.classList.add('animateLeft')
+         }
+         // @note 
+         // So the animation only plays once.
+         self.unobserve(el)
+      }
+   })
+}, {
+   // @note
+   // So the entire element has to be in view for trigger.
+   threshold: 0.5,
+});
+
+[...headings, ...skills, ...filterbtns, ...gallery].forEach(el => {
+   observer.observe(el)
+})
